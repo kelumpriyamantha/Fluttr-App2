da24f307d416327c312529615e60d415bb2c18e7
Kelum <72620573+kelumpriyamantha@users.noreply.github.com>
2020-10-11 12:03:04 +0530

    under dev

---
+++ android/app/src/main/kotlin/com/example/kh/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kh
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+
+void main() => runApp(
+  DevicePreview(
+    builder: (context) => MyApp(),
+  ),
+);
+
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      locale: DevicePreview.of(context).locale, // <--- /!\ Add the locale
+      builder: DevicePreview.appBuilder, // <--- /!\ Add the builder
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('SriLanka'),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: () {},
+          ),
+        ],
+        flexibleSpace: Icon(
+          Icons.photo_camera,
+          size:75.0,
+          color: colors.white
+        ),
+      ),
+    );
+  }
+}
